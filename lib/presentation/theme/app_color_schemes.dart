5968ed132d10f91b14d9a7c7c1eb7dac2d480a08
minKasent <247720201003@medvnu.edu.vn>
2025-08-30 01:52:44 +0700

    build codebase

---
+++ lib/presentation/theme/app_color_schemes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColorSchemes {
+  static final Color green = Color(0xFF53B175);
+  static final Color blue = Color(0xFF4A66AC);
+  static final Color lightBlue = Color(0xFF5383EC);
+  static final Color black = Color(0xFF181725);
+  static final Color white = Color(0xFFFFFFFF);
+  static final Color lightWhite = Color(0xFFF4F3FF);
+  static final Color grey = Color(0xFF7C7C7C);
+  static final Color lightGrey = Color(0xFFF2F3F2);
+}
