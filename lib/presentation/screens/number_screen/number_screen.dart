b25f36f7a2c77821367e342d1165367dc3a7c226
minKasent <247720201003@medvnu.edu.vn>
2025-09-04 23:33:59 +0700

    number_screen

--- lib/presentation/screens/number_screen/number_screen.dart
+++ lib/presentation/screens/number_screen/number_screen.dart
@@ -1,22 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_go/core/assets_gen/assets.gen.dart';
 import 'package:grocery_go/presentation/shared/app_text.dart';
+import 'package:grocery_go/presentation/shared/app_text_field_phone_number.dart';
 import 'package:grocery_go/presentation/theme/app_color_schemes.dart';
+import 'package:grocery_go/presentation/theme/app_typography.dart';
+
 class NumberScreen extends StatelessWidget {
   const NumberScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColorSchemes.lightWhite,
       appBar: AppBar(
-        actions: [
-          // Image(image: Assets)
-        ],
+        backgroundColor: AppColorSchemes.lightWhite,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+            onPressed: () => Navigator.of(context).pop(),
+        ),
+        toolbarHeight: 100,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: Assets.images.backgroundGradient.provider(),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
       ),
-      body: Column(
-        children: [
-          AppText(content: "Enter your mobile number")
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppText(
+              content: "Enter your mobile number",
+              style: AppTypography.text26w600,
+            ),
+            SizedBox(height: 20),
+            AppText(
+              content: "Mobile Number",
+              style: AppTypography.text16w600.copyWith(
+                color: Color(0xFF7C7C7C),
+              ),
+            ),
+            SizedBox(height: 10),
+            AppTextFieldPhoneNumber(),
+          ],
+        ),
       ),
     );
   }
